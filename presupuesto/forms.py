e4d5aec13ce707509d14d7ef96636d04e3473f57
yorch-dev <jltm.dev@outlook.com>
2023-03-06 19:02:21 -0300

    tabs funcionando

--- presupuesto/forms.py
+++ presupuesto/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout
 from crispy_forms.layout import Layout, Fieldset, Row, Column
 
 class Proyeccion(forms.Form):
@@ -10,4 +11,37 @@ class Proyeccion(forms.Form):
     ciclo_inversion = forms.IntegerField(required=True)
     t_int_esperada = forms.FloatField(required=True)
     t_int_inversion = forms.FloatField(required=True)
-    precision = forms.CharField(max_length=20, required=False)
+    precision = forms.TypedChoiceField(
+        label = "Precisión",
+        choices = ((0, "Baja"), (1, "Normal"), (2, "Alta"), (3, "Muy alta")),
+        widget = forms.RadioSelect,
+        initial = '1',
+        required=True
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-exampleForm'
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-5 color-white'
+        self.helper.field_class = 'col-7'
+        self.helper.form_method = 'post'
+        #self.helper.form_action = 'submit_survey'
+
+        submit = Submit("submit", "Enviar")
+        submit.field_classes = 'btn btn-dark btn-block'
+
+        #self.helper.add_input(submit)
+
+        self.helper.layout = Layout(
+            'nombre',
+            'valor',
+            'duracion',
+            'fecha_inicio',
+            'ciclo_inversion',
+            't_int_esperada',
+            't_int_inversion',
+            'precision'
+        )
+        self.helper.layout.append(submit)
